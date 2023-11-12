5a987cb08069cd875bb8093662045934cd6e9061
burakdali <burakdali@yahoo.com>
2023-11-12 23:10:04 +0300

    Main adds

--- app/Http/Controllers/CriteriaTranslationController.php
+++ app/Http/Controllers/CriteriaTranslationController.php
@@ -2,9 +2,92 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\CriteriaTranslation;
+use App\Models\Criteria;
+use App\Http\Requests\StoreCriteriaTranslationRequest;
+use App\Http\Requests\UpdateCriteriaTranslationRequest;
 
 class CriteriaTranslationController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $criteria = CriteriaTranslation::all();
+        return view('criteria.criteria', compact('criteria'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $criteria = CriteriaTranslation::all();
+        return view('criteria.create_criteria', compact('criteria'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCriteriaTranslationRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCriteriaTranslationRequest $request)
+    {
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CriteriaTranslation  $criteriaTranslation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CriteriaTranslation $criteriaTranslation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\CriteriaTranslation  $criteriaTranslation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $criteria = CriteriaTranslation::find($id);
+        return view('criteria.edit_ceiteria', compact('criteria'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCriteriaTranslationRequest  $request
+     * @param  \App\Models\CriteriaTranslation  $criteriaTranslation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $criteria = CriteriaTranslation::find($id);
+        $criteria->update($request->all());
+        return redirect()->route('criteria.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CriteriaTranslation  $criteriaTranslation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $criteria = CriteriaTranslation::find($id);
+        $criteria->delete();
+        return redirect()->route('criteria.index');
+    }
 }
--- app/Http/Controllers/FriendlySiteTranslationController.php
+++ app/Http/Controllers/FriendlySiteTranslationController.php
@@ -2,9 +2,93 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\CriteriaTranslation;
+use App\Models\Criteria;
+use App\Http\Requests\StoreCriteriaTranslationRequest;
+use App\Http\Requests\UpdateCriteriaTranslationRequest;
 
 class FriendlySiteTranslationController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $FriendlySite = FriendlySiteTranslation::all();
+        return view('Minister.Friendly_Site', compact('FriendlySite'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $FriendlySite = FriendlySiteTranslation::all();
+        return view('Minister.create_Freindly_site', compact('FriendlySite'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreFriendlySiteTranslationRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreFriendlySiteTranslationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\FriendlySiteTranslation  $friendlySiteTranslation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FriendlySiteTranslation $friendlySiteTranslation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\FriendlySiteTranslation  $friendlySiteTranslation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $FriendlySite = FriendlySiteTranslation::find($id);
+        return view('Minister.edit_friendly_site', compact('FriendlySite'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateFriendlySiteTranslationRequest  $request
+     * @param  \App\Models\FriendlySiteTranslation  $friendlySiteTranslation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $FriendlySite = FriendlySiteTranslation::find($id);
+        $FriendlySite->update($request->all());
+        return redirect()->route('Friendly_Site.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\FriendlySiteTranslation  $friendlySiteTranslation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $FriendlySite = FriendlySiteTranslation::find($id);
+        $FriendlySite->delete();
+        return redirect()->route('Friendly_Site.index');
+    }
 }
